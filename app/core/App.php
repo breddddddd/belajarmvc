42f2d684b4e822a04fd40a637a466bcdbe27efe5
bernadi sayudha <bernadisayudha@gmail.com>
2024-03-20 09:45:16 +0700

    :pertemuan 2

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,24 @@
 class App{
     public function __construct()
     {
-        echo 'berhasil masuk ke class utama App!';
+        $url = $this->parseURL();
+        var_dump ($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset ($_GET['url']))
+        {
+            //mengambil value url
+            $url = $_GET['url'];
+            //buang tanda '/' di akhir url
+            $url = rtrim($url, '/');
+            //filter url dari karakter karakter aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            //pecah url berdasarkan tanda '/' sebagai pemisah
+            $url = explode ('/', $url);
+
+            return $url;
+        }
     }
 }
